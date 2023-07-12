f6c1e1b53e49ce956ea644c2284b9612b8a2854a
Cocoa Hoto <cocoa@hoto.us>
2023-07-12 21:31:14 +0900

    fix: profile을 deep merge 하도록 구현

--- index.ts
+++ index.ts
@@ -343,7 +343,7 @@ async function getUser(userId: string): Promise<User | null> {
       throw new Error(`User file is invalid; Expected ${userId}, got ${user.misskeyId}`);
     }
 
-    return Object.assign({}, await getBaseProfile(user.baseProfile), user);
+    return mergeDeep(await getBaseProfile(user.baseProfile), user);
   } catch (e) {
     console.error(e);
 
@@ -452,4 +452,25 @@ async function sendErrorNotification(username: string, host: string, message: st
       'content-type': 'application/json',
     },
   });
+}
+
+function isObject(item: any) {
+  return (item && typeof item === 'object' && !Array.isArray(item));
+}
+function mergeDeep<T = object>(target: T, ...sources: T[]): T {
+  if (!sources.length) return target;
+  const source = sources.shift();
+
+  if (isObject(target) && isObject(source)) {
+    for (const key in source) {
+      if (isObject(source[key])) {
+        if (!target[key]) Object.assign(target, { [key]: {} });
+        mergeDeep(target[key], source[key]);
+      } else {
+        Object.assign(target, { [key]: source[key] });
+      }
+    }
+  }
+
+  return mergeDeep(target, ...sources);
 }
